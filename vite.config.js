401933c8717f537c63ea388326e38cc332bdd948
Caroline Pereira da Silva <carolinepedasil@gmail.com>
2025-06-17 20:47:44 -0300

    fix: vite config

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,9 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  server: {
-    host: '0.0.0.0',
-    port: process.env.PORT || 5173
-  },
   preview: {
     host: '0.0.0.0',
     port: process.env.PORT || 4173,
-    allowedHosts: [
-      'localhost',
-      '127.0.0.1',
-      '.onrender.com',
-      'wdd330-finalproject-doqj.onrender.com'
-    ]
+    allowedHosts: ['wdd330-finalproject-doqj.onrender.com'],
   },
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: false,
-    minify: 'terser',
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['vite']
-        }
-      }
-    }
-  }
-})
+});
