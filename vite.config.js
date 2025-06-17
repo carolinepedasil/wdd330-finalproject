65b5c64b8b4c42b14ceab3d7f7c2fde5457ce968
Caroline Pereira da Silva <carolinepedasil@gmail.com>
2025-06-17 20:58:56 -0300

    fix: render deploy

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,32 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 5173
+  },
   preview: {
     host: '0.0.0.0',
     port: process.env.PORT || 4173,
-    allowedHosts: ['wdd330-finalproject-doqj.onrender.com'],
+    allowedHosts: [
+      'localhost',
+      '127.0.0.1',
+      '.onrender.com',
+      'wdd330-final-project-jcer.onrender.com'
+    ]
   },
-});
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    minify: 'terser',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['vite']
+        }
+      }
+    }
+  },
+  appType: 'spa'
+})
